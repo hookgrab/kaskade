485320855b411754ef2e296d6fd14c76a91fe002
Atrin Hojjat <atrin.hojjat@gmail.com>
2025-04-06 15:19:59 -0400

    Add sentry setup

--- main.go
+++ main.go
@@ -1,12 +1,57 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
+
+	"github.com/getsentry/sentry-go"
+	sentryfiber "github.com/getsentry/sentry-go/fiber"
 
 	"hg.atrin.dev/kaskade/routers"
 	handlers "hg.atrin.dev/kaskade/handlers/socket"
 )
 
+func init_sentry(app *fiber.App) {
+
+	if err := sentry.Init(sentry.ClientOptions{
+		Dsn: "https://fc4d733811b0a2d8781b4a649a713377@o4506240933429248.ingest.us.sentry.io/4509107679854592",
+		TracesSampleRate: 1.0,
+	}); err != nil {
+		fmt.Printf("Sentry initialization failed: %v\n", err)
+	}
+
+	// Later in the code
+	sentryHandler := sentryfiber.New(sentryfiber.Options{
+		Repanic:         true,
+		WaitForDelivery: true,
+	})
+
+	enhanceSentryEvent := func(ctx *fiber.Ctx) error {
+		if hub := sentryfiber.GetHubFromContext(ctx); hub != nil {
+			uid := ctx.Params("uid")
+			if uid != "" {
+				hub.Scope().SetTag("req-uid", uid)
+			}
+		}
+		return ctx.Next()
+	}
+
+	app.Use(sentryHandler)
+
+	app.All("/", enhanceSentryEvent)
+
+	app.All("/", func(ctx *fiber.Ctx) error {
+		if hub := sentryfiber.GetHubFromContext(ctx); hub != nil {
+			hub.WithScope(func(scope *sentry.Scope) {
+				scope.SetExtra("unwantedQuery", "someQueryDataMaybe")
+				hub.CaptureMessage("User provided unwanted query string, but we recovered just fine")
+			})
+		}
+		return ctx.SendStatus(fiber.StatusOK)
+	})
+
+}
 
 
 
@@ -18,7 +63,11 @@ func main() {
 
 	app := fiber.New()
 
+	init_sentry(app)
+
 	routers.SetupRouters(app)
 
-	app.Listen(":4000")
+	if err := app.Listen(":4000"); err != nil {
+		panic(err)
+	}
 }
